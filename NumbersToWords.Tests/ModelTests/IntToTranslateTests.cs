df83554c2856b1c17cb76ffdddfeb732351c00ee
Jonathan Cheng <jcheng461@gmail.com>
2023-04-02 21:40:32 +0900

    WIP: refactor constructor to take different fields. WIP: redo tests for constructor

--- NumbersToWords.Tests/ModelTests/IntToTranslateTests.cs
+++ NumbersToWords.Tests/ModelTests/IntToTranslateTests.cs
@@ -5,70 +5,15 @@ using NumbersToWords.Models;
 namespace NumbersToWords.Tests
 {
   [TestClass]
-  public class TranslationTests
+  public class ToBeTranslatedAndResults
   {
     // Tests go here
-    [TestMethod]
-    public void IntToTranslateConstructor_CreatesInstanceOfIntToTranslate_IntToTranslate()
-    {
-      IntToTranslate newTranslation = new IntToTranslate(2, 1);
-      Assert.AreEqual(typeof(IntToTranslate), newTranslation.GetType());
-    }
-
-    [TestMethod]
-    public void GetOnesDigit_ReturnsOnesDigit_Int()
-    {
-      int number = 4;
-      IntToTranslate newTranslation = new IntToTranslate(number, 2);
-      int result = newTranslation.OnesDigit;
-      Assert.AreEqual(result, number);
-    }
-
-    [TestMethod]
-    public void SetOnesDigit_SetsValueOfOnesDigit_Void()
-    {
-      IntToTranslate newTranslation = new IntToTranslate(3, 9);
-      int updatedOnesDigit = 5;
-      newTranslation.OnesDigit = updatedOnesDigit;
-      Assert.AreEqual(updatedOnesDigit, newTranslation.OnesDigit);
-    }
-
-    [TestMethod]
-    public void OnesDigitToWords_ReturnsCorrespondingText_String()
-    {
-      IntToTranslate threeAsNumber = new IntToTranslate(3, 4);
-      string returnedText = threeAsNumber.NumberToText();
-      Assert.AreEqual("three", returnedText);
-    }
-
-    [TestMethod]
-    public void FiveNumberToWord_ReturnsTextFive_String()
-    {
-      IntToTranslate fiveAsNumber = new IntToTranslate(5, 7);
-      string returnedText = fiveAsNumber.NumberToText();
-      Assert.AreEqual("five", returnedText);
-    }
-
-    [TestMethod]
-    public void GetTensDigit_ReturnTensDigit_Int()
-    {
-      int number = 6;
-      IntToTranslate newTranslation = new IntToTranslate(4, 6);
-      int result = newTranslation.TensDigit;
-      Assert.AreEqual(result, number);
-    }
-
     // [TestMethod]
-    // public void GetTensDigit_ReturnsTensDigit_Int()
+    // public void ToBeTranslatedAndResultsConstructor_CreatesInstanceOfToBeTranslatedAndResults_ToBeTranslatedAndResults()
     // {
-    //   int number = 
+    //   ToBeTranslatedAndResults newTranslation = new ToBeTranslatedAndResults(2, 1);
+    //   Assert.AreEqual(typeof(ToBeTranslatedAndResults), newTranslation.GetType());
     // }
 
-    // [TestMethod]
-    // public void NumberTranslateToText_ReturnsNumbersInTextFormat_String()
-    // {
-    //   int userInputParsed = 4329;
-    //   string result = NumberTranslateToText(userInputParsed);
-    // } 
   }
 }
--- NumbersToWords/Models/IntToTranslate.cs
+++ NumbersToWords/Models/IntToTranslate.cs
@@ -3,64 +3,47 @@ using System.Collections.Generic;
 // Business logic
 namespace NumbersToWords.Models
 {
-  public class IntToTranslate
+  public class ToBeTranslatedAndResults
   {
     // Place-Value Integers
     private int _onesDigit;
-    private int _tensDigit;
 
-    // Digit Get & Set
-    public int OnesDigit
-    {
-      get { return _onesDigit; }
-      set { _onesDigit = value; }
-    }
-
-    public int TensDigit
-    {
-      get { return _tensDigit; }
-    }
 
     // Constructor (with overloaded Constructors)
-    public IntToTranslate(int onesPlace, int tensPlace)
-    {
-      _onesDigit = onesPlace;
-      _tensDigit = tensPlace;
-    }
 
-    public string NumberToText()
-    {
-      int index = _onesDigit;
-      return onesTranslation[index];
-    }
+    // public string NumberToText()
+    // {
+    //   int index = _onesDigit;
+    //   return onesTranslation[index];
+    // }
 
-    // Translators
-    Dictionary<int, string> onesTranslation = new Dictionary<int, string>()
-    {
-      { 1, "one" },
-      { 2, "two" },
-      { 3, "three" },
-      { 4, "four" },
-      { 5, "five" },
-      { 6, "six" },
-      { 7, "seven" },
-      { 8, "eight" },
-      { 9, "nine" },
-    };
+    // // Translators
+    // Dictionary<int, string> onesTranslation = new Dictionary<int, string>()
+    // {
+    //   { 1, "one" },
+    //   { 2, "two" },
+    //   { 3, "three" },
+    //   { 4, "four" },
+    //   { 5, "five" },
+    //   { 6, "six" },
+    //   { 7, "seven" },
+    //   { 8, "eight" },
+    //   { 9, "nine" },
+    // };
 
-    Dictionary<int, string> tensTranslation = new Dictionary<int, string>()
-    {
-      { 10, "ten" },
-      { 11, "eleven" },
-      { 12, "twelve" },
-      { 13, "thirteen" },
-      { 14, "fourteen" },
-      { 15, "fifteen" },
-      { 16, "sixteen" },
-      { 17, "seventeen" },
-      { 18, "eighteen" },
-      { 19, "nineteen" },
-    };
+    // Dictionary<int, string> tensTranslation = new Dictionary<int, string>()
+    // {
+    //   { 10, "ten" },
+    //   { 11, "eleven" },
+    //   { 12, "twelve" },
+    //   { 13, "thirteen" },
+    //   { 14, "fourteen" },
+    //   { 15, "fifteen" },
+    //   { 16, "sixteen" },
+    //   { 17, "seventeen" },
+    //   { 18, "eighteen" },
+    //   { 19, "nineteen" },
+    // };
 
   }
 }
